0b9e6786a99fd9425af17f1941bc0ccce5b8162c
TarekSaeed95 <110253624+TarekSaeed95@users.noreply.github.com>
2022-12-05 14:35:41 +0200

    'finish-lap'

--- src/base/pages/About.jsx
+++ src/base/pages/About.jsx
@@ -1,8 +1,18 @@
 
 function About() {
   return (
-    <div>
-      this is about page
+    <div className='container about-screen   flex items-center text-lg'
+     >
+      <div className="text-holder text-slate-200 flex flex-col gap-2">
+
+      <h2 className="text-6xl">Feedback UI
+      </h2>
+      <p>
+        A React App to get clients feedbacks, made with React, JSON server to mock server DB.
+      </p>
+      <p><span className="text-slate-500">Version:</span> 1.0.0</p>
+      <p><span className="text-slate-500">Layout By:</span> Tarek S.Ahmed</p>
+      </div>
     </div>
   )
 }
